61bf11e0d0b11f900039788f8146412e4c9c73c6
Jen Poole <contact.vandepool@gmail.com>
2018-03-28 02:18:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+// receives one argument and returns it in all caps
+function shout(string) {
+  return string.toUpperCase();
+}
+
+// receives one argument and returns it in all lowercase
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+// calls console.log() its one argument in all caps
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+// calls console.log() its one argument in all lowercase
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+
+}
