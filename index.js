adbd2a8fff0aade39aa6c7e895e5934254668448
Jen Poole <contact.vandepool@gmail.com>
2018-03-28 02:22:51 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
+// returns "I can't hear you!" if `string` is lowercase
+// returns "YES INDEED!" if `string` is uppercase
+// returns "I love you, too." if `string` is "I love you, Grandma."`
 function sayHiToGrandma(string) {
+  if ( string === string.toLowerCase() ) {
+    return "I can't hear you!";
+  } else if ( string === string.toUpperCase() ) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 
 }
